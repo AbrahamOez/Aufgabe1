7d2d383f20b9f3df936c4ed26884c0ce910b1920
Jan Lauer <Jan.Lauer@bso.int>
2023-07-17 08:06:29 +0200

    Upgedatete Version der SetBefehl Datei im bezug auf die Fehlermeldung

--- SetBefehl.java
+++ SetBefehl.java
@@ -4,6 +4,11 @@ public class SetBefehl {
       System.err.println("Es werden drei Argumente erwartet: (x) (y) ('x'/'o'); Beispiel: 'set 1 2 o'");
       return;
     }
+    if (args[3].equals("x") || args[3].equals("o")) {
+      
+    } else {
+      System.out.println("Falsche Eingabe");  
+      } // end of if-else
     CLIMain.BRETT.setFigur(Integer.parseInt(args[1]), Integer.parseInt(args[2]), args[3]);
   }
 }
